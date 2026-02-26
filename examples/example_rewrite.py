acd8b01aa5072e16a4ca9492cf8c758c0cec9bce
sati-itas <[m.gerwien@posteo.net]>
2026-02-26 14:33:32 +0100

    Add example files

--- examples/example_rewrite.py
+++ examples/example_rewrite.py
@@ -1,3 +1,24 @@
-from sparql_rdfs_rewriter.rewriter import RDFSRewriter
-from sparql_rdfs_rewriter.parser import SPARQLParser
+from sparql_rdfs_rewriter import RDFSRewriter
+from sparql_rdfs_rewriter import SPARQLParser
+from rdflib import Graph
+
+tbox = Graph()
+tbox.parse('examples/example.ttl', format='turtle')
+
+query_str = """PREFIX ex: <http://example.org/>
+            SELECT ?s ?o
+            WHERE {
+                ?s ex:memberOf ?o .
+            }
+            """
+
+rewriter = RDFSRewriter(tbox)
+rewritten_queries = rewriter.rewrite_query_str(query_str)
+parser = SPARQLParser()
+rewritten_query_string = parser.get_sparql_string(rewritten_queries)
+
+print("Original query:")
+print(query_str)
+print("\nRewritten queries:")
+print(rewritten_query_string)
 
---
+++ sparql_rdfs_rewriter/__init__.py
@@ -0,0 +1,8 @@
+from .rewriter import RDFSRewriter
+from .parser import SPARQLParser
+
+
+__all__ = [
+    "RDFSRewriter",
+    "SPARQLParser",
+]
